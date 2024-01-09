db9a87e4e66f629479400a973dad702c831a8ba6
chris shih <chris@FX8350CHRIS>
2024-01-08 18:02:50 -0800

    Believe I have added a add todo task

--- Basic-Cross-Platform-Mobile-App/Screen2_AddTask.js
+++ Basic-Cross-Platform-Mobile-App/Screen2_AddTask.js
@@ -1,5 +1,36 @@
-const AddTask = () => {
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
 
+const AddTask = ({ navigation, route }) => {
+    const [name, setName] = useState('');
+    const [task, setTask] = useState('');
+
+    const data = route.params?.data || [];
+
+    const handleAddItem = () => {
+        if (name.trim() !== '' && task.trim() !== '') {
+          const newItem = { id: data.length + 1, name, task };
+          navigation.navigate('Screen1', { data: [...data, newItem] });
+        } else {
+        }
+      };
+
+      return (
+        <View>
+          <Text>Add a New Item:</Text>
+          <TextInput
+            value={name}
+            onChangeText={(text) => setName(text)}
+            placeholder="Enter name"
+          />
+          <TextInput
+            value={task}
+            onChangeText={(text) => setTask(text)}
+            placeholder="Enter task"
+          />
+          <Button title="Add Item" onPress={handleAddItem} />
+        </View>
+      );
 };
 
 export default AddTask;
